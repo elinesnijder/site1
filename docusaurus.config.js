bdb4dc1f7cce1c8e2caba58b96a382de24f46afc
elinesnijder <a84785@ualg.pt>
2023-12-08 18:53:42 +0000

    editei docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/oficina-ware-high-resolution-logo.png',
       navbar: {
         title: 'My Site',
         logo: {
